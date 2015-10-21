083692f1d942df8b016314df0ce831853cf4005a
Reza Farid <reza.farid@roames.com.au>
2015-10-21 11:53:10 +1000

    updating examples/coloredRectItemconfig.py, fixing indent

--- examples/coloredRectItemconfig.py
+++ examples/coloredRectItemconfig.py
@@ -6,13 +6,13 @@ from sloth.items import RectItem
 class RectColor(RectItem):
     def __init__(self, model_item=None, prefix="", parent=None, color=Qt.red):
         RectItem.__init__(self, model_item, prefix, parent)
-	self._userColor=color
+        self._userColor=color
         # set drawing pen to red with width 2
         self.setPen(QPen(color, 2))
 
     def __call__(self, *args, **kwargs):
         newitem = RectItem(*args, **kwargs)
-	newitem.setColor(self._userColor)
+        newitem.setColor(self._userColor)
         return newitem
 
 # colors={1:'ColorDarkRed',2:'ColorDarkGreen',4:'ColorDarkBlue',6:'ColorDarkGray',8:'ColorRed',10:'ColorGreen',12:'ColorBlue',14:'ColorDarkCyan',16:'ColorBlack'}
